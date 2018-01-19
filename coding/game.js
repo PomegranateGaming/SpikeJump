c129cb06a866d9b810b46e4ebd0c6f6e5dfdfa68
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-01-19 16:01:28 -0500

    Update game.js

--- coding/game.js
+++ coding/game.js
@@ -1,1 +1,14 @@
-
+function clear() {
+ctx.fillStyle = "white";
+canvasJS.rectangle(0,0,500,500);
+}
+var y = 250;
+function char() {
+ctx.fillStyle = "red";
+  canvasJS.rectangle(50,y,50,50);
+  if (y < 400) {
+    y--;
+  } else {
+  y=400;
+  }
+}
