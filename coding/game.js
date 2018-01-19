47cf386f9eb8bbcc99ce5f398598221eb9389628
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-01-19 16:16:11 -0500

    Update game.js

--- coding/game.js
+++ coding/game.js
@@ -4,6 +4,7 @@ canvasJS.rectangle(0,0,500,500);
 }
 var y = 250;
 yv = 0;
+var a = 0;
 function char() {
 ctx.fillStyle = "red";
   canvasJS.rectangle(50,y,50,50);
@@ -13,9 +14,11 @@ ctx.fillStyle = "red";
   } else {
   y=400;
     yv=0;
+    a = 0;
   }
 }
-c.addEventListener("click", function(){
-    yv=-20;
-});
+ window.addEventListener('touchstart', function (e) {
+          if(a===0) { yv=-20;
+   a=1;}
+        })
 setInterval(function(){clear();char();},10);
