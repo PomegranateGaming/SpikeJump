dadb42bc3094e2f51eb6a9b8840f3a9bfeaed902
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-01-19 16:29:22 -0500

    Update game.js

--- coding/game.js
+++ coding/game.js
@@ -5,6 +5,7 @@ canvasJS.rectangle(0,0,500,500);
 var y = 250;
 yv = 0;
 var a = 0;
+var go = 0;
 function char() {
 ctx.fillStyle = "red";
   canvasJS.rectangle(50,y,50,50);
@@ -13,12 +14,13 @@ ctx.fillStyle = "red";
     y+=yv;
   } else {
   y=400;
-    yv=0;
+    if (go === 0) {
+    yv=0;} else {yv=-20;y-=20;go=0}
     a = 0;
   }
 }
         window.addEventListener('keydown', function (e) {
-              if(a===0) { yv=-20;
+              if(a===0) { go = 1;
    a=1;}
         });
 setInterval(function(){clear();char();},10);
