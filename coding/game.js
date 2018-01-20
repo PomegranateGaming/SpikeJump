b27dfdf0545fa4de600fc87e4363a9b2c629adc0
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-01-20 18:31:34 -0500

    fix bug

--- coding/game.js
+++ coding/game.js
@@ -58,16 +58,16 @@ var touch = 0;
    a=1;}
         });
 window.addEventListener('touchstart', function () {
-             if(mode===1) { if(a===0) { touch = 1;
-   a=1;}}
+              if(a===0) { touch = 1;
+   a=1;}
         });
 window.addEventListener('touchend', function () {
              if(touch===1) {go = 1; touch=0;
    }
         });
 window.addEventListener('click', function () {
-              if(mode===1) {if(a===0) {go = 1;
-  }}
+              if(a===0) {go = 1;
+  }
         });
 window.addEventListener('click', function (e) {
               if(mode===1) {} else {
