3b886c47cd93316298230b12fd762079e13c0640
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-01-20 18:09:36 -0500

    Update game.js

--- coding/game.js
+++ coding/game.js
@@ -51,7 +51,7 @@ ctx.fillStyle = "red";
 }
 var touch = 0;
         window.addEventListener('keydown', function (e) {
-              if(a===0) { go = 1;
+              if(a===0) {y=401; go = 1;
    a=1;}
         });
 window.addEventListener('touchstart', function (e) {
@@ -68,11 +68,11 @@ go = 0;
    }
         });
 window.addEventListener('touchend', function () {
-             if(touch===1) { go = 1; touch=0;
+             if(touch===1) {y=401; go = 1; touch=0;
    a=1;}
         });
 window.addEventListener('click', function (e) {
-              if(mode===1) {if(a===0) { go = 1;
+              if(mode===1) {if(a===0) {y=401; go = 1;
    a=1;}} else {
      if(e.pageY>350 && e.pageY<450 && e.pageX>50 && e.pageX<450) {
    y = 250;
