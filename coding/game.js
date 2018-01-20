ac3f8cb49c8fd296abb1705aa7396e0bd4caf065
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-01-20 17:45:28 -0500

    Update game.js

--- coding/game.js
+++ coding/game.js
@@ -44,7 +44,7 @@ ctx.fillStyle = "red";
     ctx.textAlign = "center";
   canvasJS.text.fill("50px Comic Sans MS","SpikeJump",250,210);
   canvasJS.text.fill("30px Comic Sans MS","By Pomegranate Gaming",250,250);
-    canvasJS.text.fill("30px Comic Sans MS","Click/Drag(mobile)/Press a button to start/play.",250,300);
+    canvasJS.text.fill("20px Comic Sans MS","Click/Drag(mobile)/Press a button to start/play.",250,300);
   }
 }
 var touch = 0;
