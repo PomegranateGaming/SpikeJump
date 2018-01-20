558fc87da7926790962081c2e5dcb0597acec98a
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-01-20 18:28:59 -0500

    Update game.js

--- coding/game.js
+++ coding/game.js
@@ -57,11 +57,22 @@ var touch = 0;
               if(a===0) {go = 1;
    a=1;}
         });
-window.addEventListener('touchstart', function (e) {
+window.addEventListener('touchstart', function () {
              if(mode===1) { if(a===0) { touch = 1;
-   a=1;}} else {
-     if(e.touches[0].screenY>350 && e.touches[0].screenY<450 && e.touches[0].screenX>50 && e.touches[0].screenX<450) {
-  y = 250;
+   a=1;}}
+        });
+window.addEventListener('touchend', function () {
+             if(touch===1) {go = 1; touch=0;
+   }
+        });
+window.addEventListener('click', function () {
+              if(mode===1) {if(a===0) {go = 1;
+  }}
+        });
+window.addEventListener('click', function (e) {
+              if(mode===1) {} else {
+     if(e.pageY>350 && e.pageY<450 && e.pageX>50 && e.pageX<450) {
+   y = 250;
 mode = 1;
 x = 0;
 yv = 0;
@@ -70,13 +81,8 @@ go = 0;
         }
    }
         });
-window.addEventListener('touchend', function () {
-             if(touch===1) {go = 1; touch=0;
-   }
-        });
-window.addEventListener('click', function (e) {
-              if(mode===1) {if(a===0) {go = 1;
-  }} else {
+window.addEventListener('touchstart', function (e) {
+              if(mode===1) {} else {
      if(e.pageY>350 && e.pageY<450 && e.pageX>50 && e.pageX<450) {
    y = 250;
 mode = 1;
