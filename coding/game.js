fa9c8e75d0c90652ad6e5bdaeb56a12e73d08350
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-01-20 19:40:37 -0500

    Update game.js

--- coding/game.js
+++ coding/game.js
@@ -5,6 +5,7 @@ canvasJS.rectangle(0,0,500,500);
   canvasJS.rectangle(0,450,500,50);
 }
 var y = 250;
+var lol = false;
 var mode = 0;
 var x = 0;
 yv = 0;
@@ -28,6 +29,8 @@ ctx.fillStyle = "red";
     yv=0;} else {yv=-20;y-=20;go=0;}
     
   }
+                  if (lol) {if (y===400) {go = 1;
+                         a = 1; }}
   for (var i=x-600; i<x+600; i+=300) {
   ctx.fillStyle = "gray";
   canvasJS.rectangle(i,400,50,50);
@@ -53,21 +56,26 @@ ctx.fillStyle = "red";
 }
 var touch = 0;
         window.addEventListener('keydown', function (e) {
-              if (y===400) {go = 1;
-   a=1;}
+              //if (y===400) {go = 1;
+   //a=1;}
+          lol=true;
         });
 window.addEventListener('touchstart', function () {
-              if (y===400) {touch = 1;
-   a=1;}
+             if (y===400) {touch = 1;
+   }
+  lol=true;
         });
 window.addEventListener('touchend', function () {
-             if (touch===1) {go = 1;
-                            a = 1;
-                            touch=0; }
+         if (touch===1) {go = 1;
+            //                a = 1;
+                           lol = true;
+             }
+
         });
 window.addEventListener('click', function () {
-              if (y===400) {go = 1;
-                         a = 1; }
+             // if (y===400) {go = 1;
+               //          a = 1; }
+  lol = true;
         });
 window.addEventListener('click', function (e) {
               if (mode===1) {} else {
